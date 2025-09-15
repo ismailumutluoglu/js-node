cd3119df7ee73b8aa3e47b81275ca04fa5c4a8c0
İsmail Umutluoglu <ismailumutluoglu10@gmail.com>
2025-09-15 13:58:39 +0300

    event emitter

---
+++ 04/logEvent.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+const path = require('path');
+const { format } = require('date-fns');
+const { v4: uuid } = require('uuid');
+
+
+
+const logEvents = async(message) => {
+    const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
+    const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
+    console.log(logItem);
+    try {
+        if(!fs.existsSync(path.join(__dirname,'log'))){
+            await fsPromises.mkdir(path.join(__dirname,'log'));
+        }
+        await fsPromises.appendFile(path.join(__dirname,'log','eventLog.txt'), logItem);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = logEvents;
