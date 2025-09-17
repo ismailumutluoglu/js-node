fbbb9f99057c16147114d3cd3c47497328f3781d
İsmail Umutluoglu <ismailumutluoglu10@gmail.com>
2025-09-17 17:16:34 +0300

    express middleware

---
+++ 08/server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const cors = require('cors');
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
+const PORT = process.env.PORT || 3500;
+
+// custom middleware logger
+app.use(logger);
+
+
+//THIRD PATRY MIDDLEWARE
+
+//Cross Origin Resource Sharing . (CORS); 
+const whitelist = ['https://www.google.com','http://127.0.0.1:5500','http://localhost:3500']
+const corsOptions = {
+    origin : (origin,callback) =>{
+        if(whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null,true);
+        }else{
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus : 200
+}
+app.use(cors(corsOptions));
+
+
+//built in middleware for forms
+app.use(express.urlencoded({extended : false}));
+//built in middleware for json
+app.use(express.json());
+// serve static fiels
+app.use(express.static(path.join(__dirname,'/public')));
+
+
+
+app.get('/', (req, res) => {
+    // res.sendFile('./views/index.html' , {root : __dirname}); 
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+app.get('/new-page.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
+});
+app.get('/old-page.html', (req, res) => {
+    res.redirect(301, './new-page.html'); // 302 default  ama doğru olanı 301 . 
+});
+// Route handlers 
+app.get('/hello', (req, res, next) => {
+    console.log("ahaahaa");
+    next();
+}, (req, res) => {
+    res.send("hauhauhauhau");
+})
+app.all('/pages', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
